3158738a970c4fc8a95b8cc100c8b1d0b20dd232
hanifshahy <hanifshahy@gmail.com>
2023-11-29 17:33:30 +0430

    Some registary configurations done!

--- bin/darttel.dart
+++ bin/darttel.dart
@@ -1,5 +1,23 @@
-import 'package:darttel/darttel.dart' as darttel;
+import 'dart:io' as io show File, Directory;
+import 'dart:convert' as convert show jsonDecode, jsonEncode;
 
-void main(List<String> arguments) {
-  print('Hello world: ${darttel.calculate()}!');
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+// import 'package:teledart/teledart.dart';
+
+void main() async {
+  String token = await fetchToken();
+  var username = await fetchUsername(token);
+  var teledart = TeleDart(token, Event(username ?? ''));
+}
+
+Future<String> fetchToken() async {
+  final token = convert.jsonDecode(
+      await io.File('./.regist/regist.json').readAsString())['BOT_TOKEN'];
+  return token;
+}
+
+Future<String?> fetchUsername(String token) async {
+  final username = (await Telegram(token).getMe()).username;
+  return username;
 }
